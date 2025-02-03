b1a613cd2bcdc3729e1e8fdbd85b307c6fa3ec11
sourabhdhotre09 <sourabhdhotre09@gmail.com>
2025-02-03 17:48:49 +0530

    basename added

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import Navigation from './Components/Navigation'; 
+
+const basename = "/Social_Blog_Dashboard";
 const App = () => {
   return (
-    <Router>
+    <Router basename={basename}>
       <Navigation />
       <Routes>
         <Route path="/" element={<Dashboard />} />
